2d5569156418b695cfba3bec078ed670fd8e10f8
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-11-19 11:41:27 +0530

    Final commits  (#6)
    * Update prereqs.sh
    
    * Update README.md
    
    * Update README.md
    
    * Add files via upload
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # blockTrack
 Goods transport and tracking system on Hyperledger Fabric
+
+#### Tracking Info
+<img align=center src="https://github.com/Salmandabbakuti/blockTrack/blob/master/track.png">
+
+#### Invoice Info
+<img align=center src="https://github.com/Salmandabbakuti/blockTrack/blob/master/inv.jpg">
+
+
--- client/prereqs.sh
+++ client/prereqs.sh
@@ -19,6 +19,7 @@ echo 'Registering User..'
 node registerUser.js salman
 
 echo 'All Done..'
-echo 'You can Also Use API server for Querying or invoking Calls .. Run *node server.js* and then Point to your Localhost Port 80..'
-
+echo 'You can Also Use API server for Querying or invoking Calls .. Run *node server.js salman * and then Point to your Localhost Port 80..'
+echo 'Note: If You are caking calls from outside your serving environment, you need to specify your serving environment IP Address instead Lcalhost in *app.html*..'
+echo 'Thatsall from Me..Bye..!!'
 exit 1
